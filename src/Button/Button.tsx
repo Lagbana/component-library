edd669ff74776539af6363ff5a207ee2293c0fa7
Dustin Irving <dustin.irving@gmail.com>
2021-01-09 12:28:11 -0500

    cleanup: remove test and reformat button component

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -3,8 +3,18 @@ import React from "react";
 
 import "./Button.scss";
 
-const Button: React.FC = () => (<button style={{background: "#0275d8", padding: "0.5em 1em", borderRadius:"10%", border:"none", color: "#fff"}}>Button</button>
+const Button: React.FC = () => (
+  <button
+    style={{
+      background: "#0275d8",
+      padding: "0.5em 1em",
+      borderRadius: "10%",
+      border: "none",
+      color: "#fff",
+    }}
+  >
+    Button
+  </button>
 );
 
 export default Button;
-
