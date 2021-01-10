e9a714a78891ed4d4e052287bbfdde9d38775e4f
Larry Agbana <larryagbana@gmail.com>
2021-01-10 12:31:46 -0500

    Update build

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import TestComponent from "./TestComponent";
 import Button from './Button'
 import Flex from './Flex'
 import Grid from './Grid'
 import Card from './Card'
 import { List, ListItem } from './List'
 
-export { TestComponent, Button, Flex, Grid, Card, List, ListItem };
+export { Button, Flex, Grid, Card, List, ListItem };
